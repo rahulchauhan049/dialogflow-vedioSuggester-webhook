f8d4f35d640f2a4dbe6d940366a6cd8f15c05b0a
RAHUL <rahul.chauhan049@gmail.com>
2018-12-27 01:13:04 +0530

    Added MultiLanguage
    If user's language is hindi then agent will reply in hindi.

--- functions/index.js
+++ functions/index.js
@@ -8,13 +8,14 @@ const { dialogflow, Image, SimpleResponse } = require('actions-on-google');
 const app = dialogflow({
     debug: true
 });
-
 var categoryList = [`movies`, `serials`];
 var genreList = [`action`, `adventure`, `animation`, `children`, `comedy`, `crime`, `drama`, `fantasy`, `fiction`, `historical`, `horror`, `mystery`, `romance`, `spy`, `thriller`];
 var randomCategory = categoryList[randomNumber(categoryList)];
 var randomGenre = genreList[randomNumber(genreList)];
 //handle all vedio selection part....
 app.intent("suggest-vedio", conv => {
+    const lang = conv.body.queryResult.languageCode;
+
     const category = (conv.parameters['vedioType']);
     const genre = (conv.parameters['genre']);
     return db.ref("vedios/").once("value", snapshot => {
@@ -23,24 +24,37 @@ app.intent("suggest-vedio", conv => {
         	const num = data[String(category)][String(genre)].length;
         	const random = Math.floor((Math.random() * num));
         	const toPrint = data[String(category)][String(genre)][random];
-
-        	conv.close(`Today i suggest you to watch "${toPrint}".`);
-        	
+            if(lang === 'hi'){
+                conv.close(`आज मैं आपको" ${toPrint} "देखने का सुझाव देता हूं`);
+            }
+            else{
+        	    conv.close(`Today i suggest you to watch "${toPrint}".`);
+            }
    		} else if (category.length==1 && genre.length==0){
    			const randomDataLength = Math.floor((Math.random() * data[String(category)][randomGenre].length));
     		const randomMovieWG = data[String(category)][randomGenre][randomDataLength];
-
-    		conv.close(`I think you should watch "${randomMovieWG}" today.`);
+            if(lang === 'hi'){
+                conv.close(`मुझे लगता है कि आपको आज "${randomMovieWG}" देखना चाहिए।`);
+            }else{
+                conv.close(`I think you should watch "${randomMovieWG}" today.`);
+            }
    		} else if (category.length==0 && genre.length==1){
    			const randomDataLength = Math.floor((Math.random() * data[randomCategory][String(genre)].length));
     		const randomMovieWC = data[randomCategory][String(genre)][randomDataLength];
-
-    		conv.close(`I think you should watch "${randomMovieWC}" today.`);
+            if(lang === 'hi'){
+                conv.close(`मुझे लगता है कि आपको आज "${randomMovieWC}" देखना चाहिए।`);
+            }else{
+            conv.close(`I think you should watch "${randomMovieWC}" today.`);
+            }
    		}
    		else {
     		const randomDataLength = Math.floor((Math.random() * data[randomCategory][randomGenre].length));
-    		const randomMovieWA = data[randomCategory][randomGenre][randomDataLength];
-    		conv.close(`Ummm! I think you can watch "${randomMovieWA}".`);
+            const randomMovieWA = data[randomCategory][randomGenre][randomDataLength];
+            if(lang === 'hi'){
+                conv.close(`मुझे लगता है कि आपको आज "${randomMovieWA}" देखना चाहिए।`);
+            }else{
+            conv.close(`Ummm! I think you can watch "${randomMovieWA}".`);
+            }
     	} 
         
     });
